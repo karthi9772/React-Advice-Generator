ad962225ab56cd1eb82c0f5e9b2a982c9e663dd1
karthi9772 <karthi.g9772@gmail.com>
2024-01-25 20:19:35 +0530

    Feat:Added some Api Methods For fetch Advice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Axios from "axios";
-const App = () => {
-  Axios.get("https://api.adviceslip.com/advice").then((res) => {
-    console.log(res.data.slip.advice);
-  });
 
-  return <div>Hrllośś</div>;
+const App = () => {
+  const [Data, SetData] = useState(null);
+  const [input, SetInput] = useState("");
+
+  const GetRandomAdvice = () => {
+    Axios.get("https://api.adviceslip.com/advice").then((res) => {
+      SetData(res.data.slip.advice);
+    });
+    console.log("Fetched");
+  };
+
+  const SearchAdvice = (query) => {
+    if (query !== "") {
+      Axios.get(`https://api.adviceslip.com/advice/search/${query}`).then(
+        (res) => {
+          SetData(res.data.slips[0].advice);
+        }
+      );
+    } else {
+      SetData("Enter a Valid Search");
+    }
+  };
+
+  useEffect(() => {
+    GetRandomAdvice;
+  }, []);
+
+  return (
+    <div>
+      <div className="background">
+        <h2>{Data}</h2>
+        <button onClick={() => GetRandomAdvice()}>Advice!</button>
+      </div>
+      <input value={input} onChange={(e) => SetInput(e.target.value)}></input>
+      <button onClick={() => SearchAdvice(input)}>Get input Advice</button>
+    </div>
+  );
 };
 
 export default App;
