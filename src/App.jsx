27cff9f48d8a084cdda98bc10a6558b9034b5104
karthi9772 <karthi.g9772@gmail.com>
2024-02-14 23:04:01 +0530

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ const App = () => {
   return (
     <div className="container">
       <div className="title">
-        <h1>Advice Generator</h1>
+        <h1 id="Title">Advice Generator</h1>
       </div>
       <div className="search">
         <input
